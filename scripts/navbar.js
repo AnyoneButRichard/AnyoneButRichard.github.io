382c5042ec5a288dbcf2a139e0a0be6d0a0ede43
Richard <rduon008@ucr.edu>
2020-10-05 14:43:59 -0700

    added icons and fixed navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction(){
 	var navbar = document.getElementById("navbar");
-	if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+	if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
 		navbar.style.top="0";
 	}
 	else{
