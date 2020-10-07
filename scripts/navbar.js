c864f386da0f03feaddf48935a308989a5b42c2e
Richard <rduon008@ucr.edu>
2020-10-06 20:51:38 -0700

    finished profile & partial portfolio

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction(){
 	var navbar = document.getElementById("navbar");
-	if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
+	if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
 		navbar.style.top="0";
 	}
 	else{
